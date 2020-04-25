0997d908a4763bbef9e4b0358886dcc9af0ee57d
Buttonupd <Dankariuki0101@gmail.com>
2020-04-25 20:28:13 +0300

    I wrote a function that gets json response from the url

--- app/news_test.py
+++ app/news_test.py
@@ -4,7 +4,7 @@ News = news.News
 
 class NewsTest(unittest.TestCase):
     def setUp(self):
-        self.new_news = News('CNN', 'The Wall Street Journal', 'Sumathi Reddy', 'Facts and Myths', 'describe', 'https://www.wsj.com/articles/pandemic-deepens-catholic-churchs-financial-crunch-from-vatican-to-parishes-1158773691', 'https://images.wsgi.net/im-179933/social','nything bruv' )
+        self.new_news = News('CNN', 'The Wall Street Journal', 'Sumathi Reddy', 'Facts and Myths', 'describe', 'https://www.wsj.com/articles/pandemic-deepens-catholic-churchs-financial-crunch-from-vatican-to-parishes-1158773691', 'https://images.wsgi.net/im-179933/social','nything bruv', 'as so maybe' )
 
     def test_case(self):
         self.assertTrue(isinstance(self.new_news, News))
--- app/request.py
+++ app/request.py
@@ -1,3 +1,41 @@
 from app import app
+import urllib.request
+import json
+from .models import news
+
+News = news.News
 
 api_key = app.config['NEWS_API_KEY']
+base_url = app.config['NEWS_API_BASE_URL']
+
+def get_sources(category):
+     '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(category, api_key)
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+        sources_results = None
+
+        if get_sources_response['totalResults']:
+            sources_results_list = get_sources_response['totalResults']
+            sources_results = process_results(sources_results_list)
+
+    return sources_results
+
+
+def process_results(news_list):
+      '''
+    Function  that processes the movie result and transform them to a list of Objects
+
+    Args:
+        movie_list: A list of dictionaries that contain movie details
+
+    Returns :
+        movie_results: A list of movie objects
+    '''
+
+
+
+
