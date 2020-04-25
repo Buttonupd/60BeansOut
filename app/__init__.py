f53d1758022352ab6932ecf3051a2f50a45a42eb
Buttonupd <Dankariuki0101@gmail.com>
2020-04-25 20:10:39 +0300

    Proceeded to add a configuration class and passed the NEWS url from where shall be tracked

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from app.config import DevConfig
 
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True
+)
 
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
